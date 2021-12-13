dc8e5573dc7e0c03bbe92f8b279e91c5b23ba01f
Olivier <ol.legran@gmail.com>
2021-12-13 23:34:17 +0100

    updated NLP preprocessing (P6_01 NLP.ipynb and process_text.py):convert text to lowercase, check that the tokenization removes special characters, remove numbers and uninformative sentences. Implemented preprocessing as a python script and a module. updated image preprocessing (P6_02_CV.ipynb): -check for image sizes, image aspect ratios, check for 'fake' images, look into gaussian noise (in progress), created new dataset of reduced images.

---
+++ P6/load_data.py
@@ -0,0 +1,23 @@
+#! /usr/local/bin python3
+# coding: utf-8
+"""Docstring ici pour expliquer le module"""
+
+
+import pandas as pd
+
+def load_data(path=None):
+    """docstring"""
+    
+    if path is None:
+        rootpath = "./data/Flipkart/"
+        path = rootpath + "flipkart_com-ecommerce_sample_1050.csv"
+        data = pd.read_csv(path)
+    
+    else:
+        data = pd.read_csv(path)
+
+    return data
+
+
+if __name__ == "__main__":
+    load_data()
