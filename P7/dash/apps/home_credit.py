d22f45f98b46b939cd1a65a6f9577b2a22834d62
Olivier <ol.legran@gmail.com>
2022-02-12 01:16:59 +0100

    fixed dashboard layout to have the datatable and the visualization side-by-side

--- P7/dash/apps/home_credit.py
+++ P7/dash/apps/home_credit.py
@@ -7,7 +7,10 @@ import pandas as pd
 import p7
 import lightgbm_with_simple_features as lgbmsf
 import json
-app = Dash(__name__)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 with open('./config.json', 'r') as f:
     CONFIG = json.load(f)
@@ -15,6 +18,8 @@ with open('./config.json', 'r') as f:
 PATH = CONFIG["PATH"]
 NUM_ROWS = CONFIG["NUM_ROWS"]
 
+
+# Reindexing is needed here otherwise an error is thrown when trying to select a customer
 raw_df = pd.read_csv('../data/raw_df_test.csv')
 raw_df.index = pd.Index(range(1, raw_df.shape[0]+1))
 print('raw data loaded')
@@ -34,49 +39,53 @@ app.layout = html.Div([
                 id='df-type',
             )
         ]),
-    html.Hr(),
-    html.Div([
+        html.Hr(),
+        html.H4('Choix du client'),
         html.Div([
-            html.H4('Select X axis'),
-            dcc.Dropdown(
-                value='CODE_GENDER',
-                id='xaxis-column',
-            )
-        ], style={'width':'30%'}),
-        html.Div([
-            html.H4('Select Y axis'),
-            dcc.Dropdown(
-                value='CNT_CHILDREN',
-                id='yaxis-column',
-            )
-        ], style={'width':'30%'})
-    ], style={'width':'100%','display':'flex'}),
-    html.Hr(),
-    html.H4('Choix du client'),
-    html.Div([
-        dcc.Dropdown(   
-            value=0,
-            id='client-id',
-            style={'width':'100%'}
-        ),
-        dash_table.DataTable(
-            data=[raw_df.to_dict('records')[1413]], 
-            columns=[{"name": i, "id": i} for i in raw_df.columns],
-            style_table={'width':'10%', 'overflowX': 'auto'},
-            id='client-data'
-        )
-    ], style={'width':'100%', 'display':'flex'}),
+            html.Div([
+                dcc.Dropdown(   
+                value=1,
+                id='client-id',
+                ),
+                dash_table.DataTable(
+                    style_table={'overflowX': 'auto', 'height': 400},
+                    fixed_rows={'headers': True},
+                    id='client-data'
+                )
+            ], style={'width':'45%'}),
 
-    html.Hr(),
-    html.H4('Visualisation'),
-    dcc.RadioItems(
-                ['box', 'scatter'],
-                'box',
-                id='viz-type',
-    ),
-    dcc.Graph(id='credit_default')
-
-    ], style={'width':'100%'})
+            html.Div([
+                html.Div([
+                    html.Div([
+                        html.H4('Select X axis'),
+                        dcc.Dropdown(
+                            value='CODE_GENDER',
+                            id='xaxis-column',
+                        )
+                    ], style={'width':'30%'}),
+                    html.Div([
+                        html.H4('Select Y axis'),
+                        dcc.Dropdown(
+                            value='CNT_CHILDREN',
+                            id='yaxis-column',
+                        )
+                    ], style={'width':'30%'})
+                ], style={'display':'flex'}),
+                html.Div([
+                    html.H4(
+                        'Visualisation', 
+                        style={'textAlign':'center'}
+                    ),
+                    dcc.RadioItems(
+                            ['box', 'scatter'],
+                            'box',
+                            id='viz-type'
+                    ),
+                    dcc.Graph(id='credit_default')
+                ], )
+            ], style={'width':'45%'})
+        ], style={'width':'100%', 'display':'flex'})
+    ])
 ])
 
 
@@ -124,10 +133,15 @@ def set_client_ids(selected_dataset):
     Input('client-id', 'value'),
     Input('df-type', 'value'))
 def display_client_data(selected_id, selected_dataset):
+    
     if selected_dataset == 'raw':
-        return [raw_df.to_dict('records')[selected_id]], [{"name": i, "id": i} for i in raw_df.columns]
+        data = raw_df.iloc[selected_id].T.reset_index()
     else:
-        return [df.to_dict('records')[selected_id]], [{"name": i, "id": i} for i in df.columns]
+        data = df.iloc[selected_id].T.reset_index()
+
+    data.columns = ['Feature', 'Value']
+
+    return data.to_dict('records'), [{"name": i, "id": i} for i in data.columns]
 
 
 @app.callback(
